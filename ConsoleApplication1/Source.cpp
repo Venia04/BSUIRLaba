672bcfdd671523b879f8657d8aa40e865649d08a
Veniamin Kalman <kalmanveniamin@outlook.com>
2021-09-22 01:38:10 +0300

    +проверка на ошибки

--- ConsoleApplication1/Source.cpp
+++ ConsoleApplication1/Source.cpp
@@ -7,22 +7,37 @@ double getValue(string x)
 {
     while (true)
     {   
-        cout << "Enter a double [" << x << "] value: ";
+        cout << "\nEnter a double [" << x << "] value: ";
         double a;
         cin >> a;
         if (cin.fail())
         {
             cin.clear();
             cin.ignore(32767, '\n');
+            cout << "ERROR: Not a number!\n";
         }
         else 
         return a;
     }
 }
 
-double divZero() 
+double divZero(double x) 
 {
+    if (x == 0)
+    {
+        cout << "ERROR: Divided by ZERO!\n\n";
+    }
+    else
+        return x;
+}
 
+double sqrtMinus(double x)
+{
+    if (x < 0) {
+        cout << "ERROR: sqrt from MINUS number!\n\n";
+    }
+    else
+        return x;
 }
 
 int main()
@@ -30,9 +45,11 @@ int main()
     cout << "Hello World!\n"; //test
 
     double a = getValue("a");
-    cout << "a= " << a << "\n";
+    cout << "a = " << a << "\n";
 
     double b = getValue("b");
-    cout << "b= " << b << "\n";
+    cout << "b = " << b << "\n";
+
+    divZero(b);
 
 }
